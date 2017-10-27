446fd05545655b7244faf70b60c913cb32061de0
jcup <jakubpchmiel@gmail.com>
2017-10-27 16:59:19 +0200

    Photo CardView Updated

--- app/src/main/java/com/theandroiddev/mywins/UI/Adapters/ImageSwipeAdapter.java
+++ app/src/main/java/com/theandroiddev/mywins/UI/Adapters/ImageSwipeAdapter.java
@@ -1,35 +1,67 @@
 package com.theandroiddev.mywins.UI.Adapters;
 
+import android.app.Activity;
 import android.content.Context;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Bitmap;
+import android.os.Parcelable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.download.ImageDownloader;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 import com.theandroiddev.mywins.R;
+import com.theandroiddev.mywins.UI.Views.TouchImageView;
 
-import java.io.File;
 import java.util.ArrayList;
 
+import butterknife.ButterKnife;
+
 /**
- * Created by jakub on 30.08.17.
+ * Created by jakub on 27.10.17.
  */
 
 public class ImageSwipeAdapter extends PagerAdapter {
-    private static final String TAG = "ImageSwipeAdapter";
 
+    ConstraintLayout pagerLayout;
     private Context context;
+    private LayoutInflater inflater;
+    private DisplayImageOptions options;
     private ArrayList<String> imagePaths;
 
 
     public ImageSwipeAdapter(Context context, ArrayList<String> imagePaths) {
         this.context = context;
         this.imagePaths = imagePaths;
+        inflater = LayoutInflater.from(context);
+
+        ButterKnife.bind((Activity) context);
+
+        options = new DisplayImageOptions.Builder()
+                .showImageForEmptyUri(R.drawable.ic_empty)
+                .showImageOnFail(R.drawable.ic_error)
+                .resetViewBeforeLoading(true)
+                .cacheOnDisk(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .considerExifParams(true)
+                .displayer(new FadeInBitmapDisplayer(300))
+                .build();
     }
 
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
 
     @Override
     public int getCount() {
@@ -37,36 +69,74 @@ public class ImageSwipeAdapter extends PagerAdapter {
     }
 
     @Override
+    public Object instantiateItem(ViewGroup view, final int position) {
+        View imageLayout = inflater.inflate(R.layout.item_pager_image, view, false);
+
+        final TouchImageView touchImageView = imageLayout.findViewById(R.id.image);
+        final ProgressBar spinner = imageLayout.findViewById(R.id.loading);
+
+        pagerLayout = imageLayout.findViewById(R.id.pager_image_layout);
+
+
+        ImageLoader.getInstance().displayImage(ImageDownloader.Scheme.FILE.wrap(imagePaths.get(position)), touchImageView, options, new SimpleImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                spinner.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                String message = null;
+                switch (failReason.getType()) {
+                    case IO_ERROR:
+                        message = "Input/Output error";
+                        break;
+                    case DECODING_ERROR:
+                        message = "Image can't be decoded";
+                        break;
+                    case NETWORK_DENIED:
+                        message = "Downloads are denied";
+                        break;
+                    case OUT_OF_MEMORY:
+                        message = "Out Of Memory error";
+                        break;
+                    case UNKNOWN:
+                        message = "Unknown error";
+                        break;
+                }
+                Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+
+                spinner.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                spinner.setVisibility(View.GONE);
+                //JCUP- strange bug (I have to set touchImageView visibility to GONE and change it to VISIBLE
+                //after ProgressBar is set to GONE. Otherwise it won't work
+                touchImageView.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        view.addView(imageLayout);
+        return imageLayout;
+    }
+
+    @Override
     public boolean isViewFromObject(View view, Object object) {
-        return view == object;
+        return view.equals(object);
     }
 
     @Override
-    public Object instantiateItem(ViewGroup container, int position) {
-        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View item_view = layoutInflater.inflate(R.layout.image_big, container, false);
-        ImageView imageView = item_view.findViewById(R.id.image_big);
-
-        String path = imagePaths.get(position);
-        Picasso.with(context).load(new File(path)).into(imageView);
-
-        container.addView(item_view);
-        return item_view;
-
+    public void restoreState(Parcelable state, ClassLoader loader) {
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
-
-        View view = (View) object;
-        ImageView imageView = view.findViewById(R.id.image_big);
-        BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
-        if (bitmapDrawable != null && bitmapDrawable.getBitmap() != null) {
-            bitmapDrawable.getBitmap().recycle();
-        }
-        container.removeView(view);
-
+    public Parcelable saveState() {
+        return null;
     }
-
 
 }
---
+++ app/src/main/java/com/theandroiddev/mywins/UI/Fragments/MainFragment.java
@@ -0,0 +1,10 @@
+package com.theandroiddev.mywins.UI.Fragments;
+
+/**
+ * Created by jakub on 27.10.17.
+ */
+
+public class MainFragment {
+
+
+}
