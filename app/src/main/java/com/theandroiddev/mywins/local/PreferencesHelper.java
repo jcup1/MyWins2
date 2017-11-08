826fe566dee7eaedfcd9c7040cc6a2532fe09212
jcup1 <jakubpchmiel@gmail.com>
2017-11-08 18:41:45 +0100

    SharedPref changed

--- app/src/main/java/com/theandroiddev/mywins/local/PreferencesHelper.java
+++ app/src/main/java/com/theandroiddev/mywins/local/PreferencesHelper.java
@@ -6,24 +6,31 @@ import android.content.SharedPreferences;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import static com.theandroiddev.mywins.UI.Helpers.Constants.PACKAGE_NAME;
+
 /**
  * Created by jakub on 04.11.17.
  */
 
 @Singleton
 public class PreferencesHelper {
-    //TODO IMPLEMENT?
 
-    public static final String PREF_FILE_NAME = "android_mywins_pref_file";
-
-    private final SharedPreferences mPref;
+    private final SharedPreferences pref;
 
     @Inject
     public PreferencesHelper(@ApplicationContext Context context) {
-        mPref = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        pref = context.getSharedPreferences(PACKAGE_NAME, Context.MODE_PRIVATE);
     }
 
     public void clear() {
-        mPref.edit().clear().apply();
+        pref.edit().clear().apply();
+    }
+
+    public boolean isFirstSuccessAdded() {
+        return pref.getBoolean("firstsuccess", false);
+    }
+
+    public void setFirstSuccessAdded() {
+        pref.edit().putBoolean("firstsuccess", true).apply();
     }
 }
