ea279b39f3c1786be60fa9a0f9e2ba54d795f859
jakubpchmiel@gmail.com <jakubpchmiel@gmail.com>
2018-08-05 14:16:52 +0200

    Convert ApplicationContext to Kotlin

---
+++ app/src/main/java/com/theandroiddev/mywins/injection/ApplicationContext.kt
@@ -0,0 +1,12 @@
+package com.theandroiddev.mywins.injection
+
+
+import javax.inject.Qualifier
+
+/**
+ * Created by jakub on 04.11.17.
+ */
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationContext
